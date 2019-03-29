7b4b8efbf1020fd198f57eba0f9985d504067971
Greg Rundlett <greg@freephile.com>
2019-03-29 15:12:29 -0400

    importing original code

---
+++ FsApiExtension.i18n.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright Copyright (c) 2012, Intellectual Reserve, Inc.
+ * http://www.familysearch.org
+ * Basic Api functionality
+ *
+ *
+ * @name:    FsApiExtension.i18n
+ * @purpose: Translate any necessary phrases
+ * @author:  Andy Olsen <andy@rfocus.com>
+ * @version: 0.0.1
+ * @date:    31 Dec 2012
+ */
---
+++ FsApiExtension.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @copyright Copyright (c) 2012, Intellectual Reserve, Inc.
+ * http://www.familysearch.org
+ * Basic Api functionality
+ *
+ *
+ * @name:    FsApiExtension
+ * @purpose: Interface to mediawiki include process
+ * @author:  Andy Olsen <andy@rfocus.com>
+ * @version: 0.0.1
+ * @date:    31 Dec 2012
+ */
+
+require_once('includes/global_api.php');
+require_once('extensions/FsApi/FsApiImpl.php');
+require_once('extensions/FsApi/MwApiImpl.php');
+require_once('extensions/FsApi/FsCurl.php');
+
+$wgExtensionCredits['other'][] =
+	array(
+		'name' => 'FSApi',
+		'author' => 'Andy Olsen',
+		'version' => '0.0.1',
+		'url' => 'https://familysearch.org/wiki/en/extensions/FsApi/FsApiExtension.php',
+		'description' => 'An api centered toward FamilySearch needs.'
+	);
+
+
+// Create a network transport
+$fsTransport = new FsCurl();
+
+// Pulled in from includes/global_api.php
+$fsApiImpl = new FsApiImpl($fsTransport, FS_WIKI_FS_API_URL, FS_WIKI_MW_API_URL, FS_WIKI_INDEX_URL);
+
+// extract the api action
+$outputXml = $fsApiImpl->runQuery($_REQUEST);
+
+header('Content-Type: text/xml');
+header('Content-Length: ' . strlen($outputXml));
+print $outputXml;
