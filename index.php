c569ce2c738fa31aa954d281e03ce333874958ca
Greg Rundlett <greg@equality-tech.com>
2019-04-22 13:12:54 +0000

    fully commented the code

--- index.php
+++ index.php
@@ -11,30 +11,43 @@
  * @version: 1.0.0
  * @date: 2019/04/19
  */
+// this is the page we want to parse for data
 define ("FHC_MASTER_LIST", 137044);
+
+// take in our parameters from the request
 $language = $_GET["language"]; // 'en'
+// default to 'en' for language
 $language = (empty($language))? "en" : $language;
 $unitNum = $_GET["unitNbr"]; // 3331776
 
-$url = "https://beta.familysearch.org/wiki/$language/api.php" .
+// Full URI for API endpoint
+$url = "https://{$_SERVER['HTTP_HOST']}/wiki/$language/api.php" .
 	"?format=json&action=query&pageids=" . FHC_MASTER_LIST . "&prop=revisions&rvprop=content";
 
+// We requested json because it's easier to work with
 $json = file_get_contents( $url );
-
+// json_decode turns it into a PHP data structure
 $obj = json_decode($json, true);
 
+// intialize our return value
 $ret = 'not found';
+// and grab the string contents of the PHP object
 $str = $obj["query"]["pages"]["137044"]["revisions"][0]["*"];
 
+// Turn contents into an array based on newlines
 $ary = explode("\n", $str);
 
+// The values each have to be split as well
 foreach ( $ary as $k => $v ) {
 	$v = explode( '|', $v );
-
+	// find the key we're looking for
 	if ( in_array ( "#$unitNum", $v ) ) {
 		$ret = $v[1];
 	}
 }
 
-$xml = new SimpleXMLElement("<api><url><rev>$ret</rev></url></api>");
+// Turn our response into XML to meet existing system requirements
+$xml = new SimpleXMLElement("<?xml version=\"1.0\" encoding=\"utf-8\"?><api><urls><rev>$ret</rev></urls></api>");
+
+// Return the XML document
 echo $xml->asXML();
