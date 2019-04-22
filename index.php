efa61fb9a6c5626dd2ba2f8d51ff3e4caf0e5b9d
Greg Rundlett <greg@equality-tech.com>
2019-04-22 01:04:59 +0000

    final workaround for ApiBase not found

--- index.php
+++ index.php
@@ -16,21 +16,25 @@ $language = $_GET["language"]; // 'en'
 $language = (empty($language))? "en" : $language;
 $unitNum = $_GET["unitNbr"]; // 3331776
 
-
 $url = "https://beta.familysearch.org/wiki/$language/api.php" .
 	"?format=json&action=query&pageids=" . FHC_MASTER_LIST . "&prop=revisions&rvprop=content";
+
 $json = file_get_contents( $url );
+
 $obj = json_decode($json, true);
 
 $ret = 'not found';
 $str = $obj["query"]["pages"]["137044"]["revisions"][0]["*"];
+
 $ary = explode("\n", $str);
 
 foreach ( $ary as $k => $v ) {
 	$v = explode( '|', $v );
-	// print_r ($v);
+
 	if ( in_array ( "#$unitNum", $v ) ) {
 		$ret = $v[1];
 	}
 }
-echo $ret;
+
+$xml = new SimpleXMLElement("<api><url><rev>$ret</rev></url></api>");
+echo $xml->asXML();
